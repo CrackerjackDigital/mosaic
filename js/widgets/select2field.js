6d9dd1a198ab2664fc10c0c0262d389a539dd15f
Steve Wake <stephen.wake@careers.govt.nz>
2015-03-13 11:46:23 +1300

    Placeholder select2field and added error and warning styles

--- js/widgets/select2field.js
+++ js/widgets/select2field.js
@@ -3,13 +3,14 @@
 		var $this = $(this),
 			seperator = $this.attr('tagseperator') ? $this.attr('tagseperator') : ',',
 			tags = $this.attr('tags') ? $this.attr('tags').split(seperator) : false,
+			placeholder = $this.attr('placeholder')
 			options = tags ? {
 				tags: tags,
-				tokenSeparators: [ seperator ]
+				tokenSeparators: [ seperator ],
+				placeholder: placeholder
 			} : {
+				placeholder: placeholder
 			};
-
-		console.log(options);
 
 		// add options to options map which may contain tags if tags attribute set.
 		$this.select2(
