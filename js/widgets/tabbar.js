ecec6013a36a5c23ce7bff34c6c2437761772125
Nathaniel Ashiagbor <natino45@gmail.com>
2015-06-01 13:21:39 +0000

    added cookie to navtabs - page remembers last clicked tab

--- js/widgets/tabbar.js
+++ js/widgets/tabbar.js
@@ -1,17 +1,28 @@
 (function() {
 	// if we have tab-bar handle hiding/showing tab-body with ID
 	if ($('.tab-bar').length) {
-		var first = $('.tab-bar > ul li:first-child a'),
-			rel = first.attr('rel');
+		if (getCookie('TabHistory') == null) {
+			console.log('no past tab clicked ');
+			var first = $('.tab-bar > ul li:first-child a'),
+				rel = first.attr('rel');
 
-		// select the tab
-		first.closest('li').addClass('current');
+			// select the tab
+			first.closest('li').addClass('current');
 
-		// show the first tab body
-		$(rel).addClass('current');
-
+			// show the first tab body
+			$(rel).addClass('current');
+		}else{
+			var recentTabId = getCookie('TabHistory');
+			console.log('recent tab: ' + recentTabId);
+			var recentTab = $("#" + recentTabId +' a'),
+				rel = recentTab.attr('rel');
+				$("#" + recentTabId).addClass('current');
+				$(rel).addClass('current');
+		}
 
 		$('.tab-bar > ul li').on('click', function() {
+			//clicked tab id
+			var tabId = $(this).attr('id');
 			// remove current from list item
 			$('.tab-bar > ul li').removeClass('current');
 
@@ -23,6 +34,24 @@
 
 			// show the tab-body for clicked item
 			$($('a', $(this)).attr('rel')).addClass('current');
+
+			//set cookie on clicked tab
+			setCookie('TabHistory', tabId);
 		});
 	}
+
+//Cookies
+function setCookie(key, value) {
+	var expires = new Date();
+	expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+	document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+	var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+	return keyValue ? keyValue[2] : null;
+}
+
 })();
+
+
