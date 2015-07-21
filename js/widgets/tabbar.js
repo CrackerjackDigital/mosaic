36850c16d174e90fd433f1ae4e5012c04754748a
Nathaniel Ashiagbor <nathaniel@moveforward.co.nz>
2015-07-21 17:07:12 +0000

    added tab click history to url

--- js/widgets/tabbar.js
+++ js/widgets/tabbar.js
@@ -1,57 +1,55 @@
 (function() {
-	// if we have tab-bar handle hiding/showing tab-body with ID
-	if ($('.tab-bar').length) {
-		if (getCookie('TabHistory') == null) {
-			console.log('no past tab clicked ');
-			var first = $('.tab-bar > ul li:first-child a'),
-				rel = first.attr('rel');
+    // if we have tab-bar handle hiding/showing tab-body with ID
+    if ($('.tab-bar').length) {
+        //get tab id from hash
+        var selectedTab = window.location.hash.substr(1);
+        if (selectedTab !='' && selectedTab.substring(0, 4) == 'tab_') {
 
-			// select the tab
-			first.closest('li').addClass('current');
+            var recentTabId = selectedTab;
+            var recentTab = $("#" + recentTabId + ' a'),
+                rel = recentTab.attr('rel');
+            $("#" + recentTabId).addClass('current');
+            $(rel).addClass('current');
+        } else {
+            var first = $('.tab-bar > ul li:first-child a'),
+                rel = first.attr('rel');
 
-			// show the first tab body
-			$(rel).addClass('current');
-		}else{
-			var recentTabId = getCookie('TabHistory');
-			console.log('recent tab: ' + recentTabId);
-			var recentTab = $("#" + recentTabId +' a'),
-				rel = recentTab.attr('rel');
-				$("#" + recentTabId).addClass('current');
-				$(rel).addClass('current');
-		}
+            // select the tab
+            first.closest('li').addClass('current');
 
-		$('.tab-bar > ul li').on('click', function() {
-			//clicked tab id
-			var tabId = $(this).attr('id');
-			// remove current from list item
-			$('.tab-bar > ul li').removeClass('current');
+            // show the first tab body
+            $(rel).addClass('current');
+        }
 
-			// add current to this list item
-			$(this).addClass('current');
+        $('.tab-bar > ul li').on('click', function(e) {
+            //clicked tab id
+            var tabId = $(this).attr('id');
+            // remove current from list item
+            $('.tab-bar > ul li').removeClass('current');
 
-			// remove current from all tab bodies
-			$('.tab-body').removeClass('current');
+            // add current to this list item
+            $(this).addClass('current');
 
-			// show the tab-body for clicked item
-			$($('a', $(this)).attr('rel')).addClass('current');
+            // remove current from all tab bodies
+            $('.tab-body').removeClass('current');
 
-			//set cookie on clicked tab
-			setCookie('TabHistory', tabId);
-		});
-	}
+            // show the tab-body for clicked item
+            $($('a', $(this)).attr('rel')).addClass('current');
+            e.preventDefault();
+            window.location.hash = tabId;
+        });
+    }
 
-//Cookies
-function setCookie(key, value) {
-	var expires = new Date();
-	expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
-	document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-}
+    //Cookies
+    function setCookie(key, value) {
+        var expires = new Date();
+        expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+    }
 
-function getCookie(key) {
-	var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-	return keyValue ? keyValue[2] : null;
-}
+    function getCookie(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        return keyValue ? keyValue[2] : null;
+    }
 
 })();
-
-
