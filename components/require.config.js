2a852973985d11634a6f518e4175808b2ec36221
Steve Wake <stephen.wake@careers.govt.nz>
2015-10-20 11:45:00 +1300

    Added components to git so don't have to composer update

---
+++ components/require.config.js
@@ -0,0 +1,45 @@
+var components = {
+    "packages": [
+        {
+            "name": "fotorama",
+            "main": "fotorama-built.js"
+        },
+        {
+            "name": "jquery",
+            "main": "jquery-built.js"
+        },
+        {
+            "name": "masonry",
+            "main": "masonry-built.js"
+        },
+        {
+            "name": "modernizr",
+            "main": "modernizr-built.js"
+        },
+        {
+            "name": "lodash",
+            "main": "lodash-built.js"
+        },
+        {
+            "name": "postal.js",
+            "main": "postal.js-built.js"
+        }
+    ],
+    "shim": {
+        "masonry": {
+            "exports": "Masonry"
+        },
+        "modernizr": {
+            "exports": "window.Modernizr"
+        }
+    },
+    "baseUrl": "components"
+};
+if (typeof require !== "undefined" && require.config) {
+    require.config(components);
+} else {
+    var require = components;
+}
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = components;
+}
