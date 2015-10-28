b08a579969215ea5fbfc13d9f155de12cf8e498d
Steve Wake <stephen.wake@careers.govt.nz>
2015-10-29 12:15:44 +1300

    update update script to change to script directory to make automation easier

--- update-components.sh
+++ update-components.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "1/3: setting up environment"
-if [ !-d vendor ]; then
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir
+echo "In directory `pwd`"
+if [ !-d "vendor" ]; then
 	mkdir vendor
 fi
 echo "2/3: deleting existing components"
@@ -9,4 +12,5 @@ echo "please don't build me SilverStripe" > vendor/_manifest_exclude
 export COMPOSER=components.json;
 echo "3/3: updating components using $COMPOSER"
 composer update --prefer-dist --no-dev
+cd -
 echo "done"
